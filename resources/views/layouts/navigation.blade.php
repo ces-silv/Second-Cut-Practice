622e0fe57826bb0863447ff84c27e161719698b1
NotMacaco22 <casilva@uamv.edu.ni>
2025-05-12 03:55:25 -0600

    navbar edited

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,7 +15,14 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('ferias.index')" :active="request()->routeIs('ferias.*')">
+                        {{ __('Ferias') }}
+                    </x-nav-link>
+                    <x-nav-link :href="route('emprendedores.index')" :active="request()->routeIs('emprendedores.*')">
+                        {{ __('Emprendedores') }}
+                    </x-nav-link>
                 </div>
+
             </div>
 
             <!-- Settings Dropdown -->
@@ -66,11 +73,18 @@
 
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
-        <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
-            </x-responsive-nav-link>
-        </div>
+    <div class="pt-2 pb-3 space-y-1">
+    <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+        {{ __('Dashboard') }}
+    </x-responsive-nav-link>
+    <x-responsive-nav-link :href="route('ferias.index')" :active="request()->routeIs('ferias.*')">
+        {{ __('Ferias') }}
+    </x-responsive-nav-link>
+    <x-responsive-nav-link :href="route('emprendedores.index')" :active="request()->routeIs('emprendedores.*')">
+        {{ __('Emprendedores') }}
+    </x-responsive-nav-link>
+</div>
+
 
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
