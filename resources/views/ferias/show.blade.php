1096b11a1b5c7befb2f95e6f305203a3926de4f5
NotMacaco22 <casilva@uamv.edu.ni>
2025-05-12 03:59:25 -0600

    see entrepreneurs

--- resources/views/ferias/show.blade.php
+++ resources/views/ferias/show.blade.php
@@ -10,6 +10,19 @@
     <div class="mb-3"><strong>Dirección:</strong> {{ $feria->address }}</div>
     <div class="mb-3"><strong>Descripción:</strong><br> {{ $feria->description }}</div>
 
+    <div class="mb-3">
+        <strong>Emprendedores participantes:</strong>
+        @if($feria->emprendedores->isEmpty())
+            <p class="text-muted">No hay emprendedores asignados a esta feria.</p>
+        @else
+            <ul class="list-group">
+                @foreach($feria->emprendedores as $emprendedor)
+                    <li class="list-group-item">{{ $emprendedor->name }}</li>
+                @endforeach
+            </ul>
+        @endif
+    </div>
+
     <a href="{{ route('ferias.index') }}" class="btn btn-secondary">Volver</a>
 </div>
 @endsection
